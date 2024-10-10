64dbad3a8098bccd78b76fea0d978041797e3a2b
Olly <56711484+Wologman@users.noreply.github.com>
2024-10-10 14:27:45 +1300

    Add files via upload

--- Python/test.py
+++ Python/test.py
@@ -1,67 +1,91 @@
+#To run from terminal, using the environment file:  export $(grep -v '^#' .env | xargs) && python3 test.py
+import sys
+print(sys.executable)
+
 import os, sys, time
 
-#CURRENTLY THE WAY TO SET DYNAMIC LINKS TEMPORARILY FIRST IS    export $LD_LIBRARY_PATH=/usr/lib    
-#The developer cookbook says we need to do this, but I'm not getting any import errors, so maybe not
-#Or maybe this is needed if I want to use a different python interpreter
+'''
+This is from sys.path inside qgis console:
+    '/usr/share/qgis/python', 
+    '/home/olly/.local/share/QGIS/QGIS3/profiles/default/python', 
+    '/home/olly/.local/share/QGIS/QGIS3/profiles/default/python/plugins', 
+    '/usr/share/qgis/python/plugins', 
+    '/usr/lib/python3.10',
+    '/usr/lib/python3/dist-packages', 
+    '/home/olly/.local/share/QGIS/QGIS3/profiles/default/python']
 
-ld_library_path = os.environ.get('LD_LIBRARY_PATH', '') #Will be empty if the above step not done
+sys.path: /usr/bin/python3
+sys.version: 3.12.3 (main, Sep 11 2024, 14:17:37) [GCC 13.2.0]
 
-#print(f'LD_LIBRARY_PATH: {ld_library_path}')
-#print('PYTHONPATH:', os.environ['PYTHONPATH'])
-#print(sys.executable)  in qgis console I get:    '/usr/bin/python3'  The imports work as expected if I switch from my coda enironment to this interpreter.
+To make this work:
+    (1)  The extra pythonpaths need to be in our PYTHONPATH environment
+    (2)  LD_LIBRARY_PATH from both QGIS and Conda need to be added to LD_LIBRARY_PATH
+    (3)  We also need to add any extra paths for our Conda environment to PYTHONPATH, but without actually activating it
+    (4)  The interpreter path for the environmnet must be the same one that QGIS uses
+    (5)  Where there is overlap between conda and QGIS dependencies, try to force the same version for Conda both (eg python its self)
 
-#Now to set the PYTHONPATH
-#This is from sys.path inside qgis console:
-extra_paths = ['/usr/share/qgis/python', 
-               '/home/olly/.local/share/QGIS/QGIS3/profiles/default/python', 
-               '/home/olly/.local/share/QGIS/QGIS3/profiles/default/python/plugins', 
-               '/usr/share/qgis/python/plugins', 
-               '/usr/lib/python3.10',
-               '/usr/lib/python3/dist-packages', 
-               '/home/olly/.local/share/QGIS/QGIS3/profiles/default/python']
+To run in from terminal, go to the folder holding the scripts and the .env, then:
+    source .env                             (Setus up all the paths and stores in environment variables)
+    $PYTHON_INTERPRETER my_test_script.py   (Uses the interpreter from the .env to run the Python script)
+    
+To make this work in a fancy IDE:
+    - The IDE settings need to recognise steps (1),(2),(3),(4) for linting, debugging, launch.  
+    - In VSCode, those are all seperate things, and you can't just set the conda environment like normal, or it over-rides the .env
+'''
 
-for path in extra_paths:
-    if path not in sys.path:
-        sys.path.append(path)
-        print(f'Appending {path} to sys.path')
-print('sys.path:', sys.path)
-
-
-#This is isn't the Pythonic way, and it doesn't help our editor recognise the extra packages for syntax assistance
-#The VSCode way to do this is to create a settings.json for the editing with the extra paths
-#and also a launch.json to launch the script uising for the extra paths.  Both sit under the project workspace for transparency.
-#Actually, better still would be to store all this in the .env file, then change settings.json and launch.json to refer to that.
-#PyCharm and presumably Spyder would directly recognise the .env and add the relevant paths.  I should check this.
-
-
-import qgis  #Only works with the same interpreter as qgis its self. So QGIS is putting useful links in with this interpreter somehow?
-from qgis.gui import *   #ModuleNotFoundError: No module named 'qgis._gui'   when using the interpreter from Conda
+from qgis.gui import *
 from qgis.core import *
 from qgis.utils import plugins
 from qgis.analysis import QgsNativeAlgorithms 
 from PyQt5.QtCore import *
- 
 
+#conda imports
+import cv2
+import piexif 
+import matplotlib
+import pandas as pd
+import geopandas as gpd
+import sklearn as skl
+import scipy
+import shapely
+import cartopy
+import folium
+from PIL import Image
+import geoviews
+import dotenv
+import plotly.express as px
+import arrow
+import yaml
+import fastparquet
+import xmltodict
+
+#Troublemakers:
+#import ipyleaflet   # TypeError: type 'List' is not subscriptable 
+#import rasterio   `LIBTIFF_4.6.1' not found
+#import rasterstats  `LIBTIFF_4.6.1' not found
+#import contextily  # version `LIBTIFF_4.6.1' not found 
+#import seaborn  # TypeError: type 'List' is not subscriptable  #This could be an issue with using python 3.12
+
+
+#QGIS
 QgsApplication.setPrefixPath('/usr', True)
 app = QgsApplication([], False)
 app.initQgis()                
- 
-#To create and work with a project
+
 project = QgsProject.instance()
-#project.read('testdata/01_project.qgs')
-#print(project.fileName())
-#Do some stuff
-#project.write()
-#project.write('testdata/my_new_qgis_project.qgs')
+project.read('testdata/01_project.qgs')
+print(project.fileName())
+
+#Do some actual data stuff
+project.write()
+project.write('testdata/my_new_qgis_project.qgs')
 
 QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
-#for alg in QgsApplication.processingRegistry().algorithms():
-#       print(alg.id(), "--->", alg.displayName())
+for alg in QgsApplication.processingRegistry().algorithms():
+       print(alg.id(), "--->", alg.displayName())
 
-from qgis import processing
-from processing.core.Processing import Processing  #What is going on here with PyLance?  Need to sort out settings.json
-Processing.initialize()
-
+#Do something with the processing module
+from processing.core.Processing import processing
 
 shapefile_path = '/media/olly/Blue Samsung SSD/Documents/GIS/Notebooks/Data/kx-doc-huts-SHP/doc-huts.shp'
 input_layer = QgsVectorLayer(shapefile_path, 'input_layer', 'ogr')  
@@ -74,6 +98,7 @@ processing.run("native:buffer", {
 })
 
 app.exit()
+
 #app.exitQgis()   #Segmentation fault (core dumped) #Happens if I have opened a layer.  
 #The develeper cookbook doesn't discuss this.
 #app = QgsApplication([], False) a #Segmentation fault (core dumped)  #Same problem
@@ -87,3 +112,5 @@ second_app.initQgis()
 # https://docs.qgis.org/3.34/en/docs/pyqgis_developer_cookbook/intro.html#using-pyqgis-in-custom-applications 
 #To do this we still need to construct the gui. But this could be really useful at DOC.
 second_app.exit()
+
+print('\033[1m' + '\033[94m' + "Awesome, success, God damn it Gump you're goddamn genius" + '\033[0m')
